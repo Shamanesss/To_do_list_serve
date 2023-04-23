068579ae22f6089246e94007ef5fc6793f24c4eb
Shamanesss <mentulbenodorna@gmail.com>
2023-04-23 12:53:39 +0200

    2

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ server.use(middlewares);
 server.use(router);
 //y esta es la 2 linea
 server.use(cors()); 
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://myfist-json-serve.herokuapp.com/");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
