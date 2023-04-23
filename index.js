0c4c28851250aa8e9c06c73798d56d788c5de9b9
Shamanesss <mentulbenodorna@gmail.com>
2023-04-23 10:37:44 +0200

    .

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 
-// const data =('db.json')
+
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router();
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
-
-// server.use(cors())
 
 server.use(middlewares);
 server.use(router);
@@ -14,19 +12,5 @@ server.use(router);
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
-
-
-//solucionar Problema con las cors
-// const express = require('express');
-// const cors = require('cors');
-
-// const app = express();
-
-// app.use(cors({
-//   origin: '*',
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204
-// }));
 
 
