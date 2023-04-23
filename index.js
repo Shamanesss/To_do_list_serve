cc61bae533c4db9ec80be499b0296caca280aca5
Shamanesss <mentulbenodorna@gmail.com>
2023-04-23 12:24:57 +0200

    ,

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 
 const jsonServer = require("json-server");
+//la siguiente linea es de cors sin este llaman
+const cors = requiere('cors');
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
@@ -8,6 +10,8 @@ const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+//y esta es la 2 linea
+server.use(cors()); 
 
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
